41352d53b882249489642c7d5fa476a37efc4284
Santiagojihe <sjimenezhe@unal.edu.co>
2022-10-31 20:48:47 -0500

    iu

---
+++ IUejemplo3/src/iuejemplo3/Pitagoras.java
@@ -0,0 +1,15 @@
+package iuejemplo3;
+
+/**
+ *
+ * @author santi
+ */
+public class Pitagoras {
+    public static double calcular_hipotenusa(double a, double b){
+    double c;
+    c= Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+    return c;
+    
+    }
+    
+}
